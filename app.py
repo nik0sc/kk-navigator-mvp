60f1b8941db012ea6887d2f5634f0c190584ebcc
Emrys-Hong <hongpengfei.emrys@gmail.com>
2018-09-28 22:52:58 +0800

    init app.py is functional. Ckp 1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from table import table
+import sutd_navigation
 
 app = Flask(__name__)
 
@@ -9,6 +10,9 @@ def hello():
     return 'Hello, world'
 
 
-@app.route('/directions/<start>/<dest>')
+@app.route('/directions/<start>/<dest>', methods=['GET'])
 def directions(start, dest):
-    return jsonify(table.get((start, dest), 'No directions found'))
+    return jsonify({"direction": sutd_navigation.generate_route(sutd_navigation.g.shortest_path(start, dest))})
+
+if __name__ == "__main__":
+    app.run(debug=True)
