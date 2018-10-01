51dcf618e9b274b960dc2c37e66fb4083e9d24d2
Timothy Liu <tlkh@live.co.uk>
2018-10-01 19:04:21 +0800

    Change to POST

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import navigation
 from map_render import *
 
@@ -9,18 +9,28 @@ app = Flask(__name__)
 mqtt_service = MQTT()
 
 
-@app.route('/directions/<start>/<dest>', methods=['GET'])
-def directions(start, dest):
+@app.route("/directions", methods=["POST"])
+def directions():
 
-    try:
-        nodes = navigation.g.shortest_path(start, dest)
-        direction = navigation.generate_route(nodes)
-        map_render = str(render_map(nodes))
-        mqtt_service.publish_msg(str(nodes))
-        error_code = ""
+    direction = ""
+    map_render = ""
+    error_code = ""
 
-    except Exception as e:
-        error_code = str(e)
+    if request.method == "POST":
+        try: 
+            start = request.args.get("start_loc")
+            dest = request.args.get("end_loc")
+
+            try:
+                nodes = navigation.g.shortest_path(start, dest)
+                direction = navigation.generate_route(nodes)
+                map_render = str(render_map(nodes))
+                mqtt_service.publish_msg(str(nodes))
+            except Exception as e:
+                error_code = "Routing Error: " + str(e)
+
+        except Exception as e:
+            error_code = "Arguments Error: " + str(e)
 
     response = {
         "direction": direction,
@@ -32,4 +42,5 @@ def directions(start, dest):
 
 
 if __name__ == "__main__":
+    rint("[INFO ] Starting Flask server")
     app.run(debug=True)
